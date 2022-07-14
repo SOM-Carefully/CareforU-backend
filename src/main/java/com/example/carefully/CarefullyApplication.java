a3a647b0f47b73dec06236980474ddbc30950858
semi-cloud <sksmsrkdtpal@naver.com>
2022-07-14 23:30:03 +0900

    :tada: 프로젝트 생성

---
+++ src/main/java/com/example/carefully/CarefullyApplication.java
@@ -0,0 +1,13 @@
+package com.example.carefully;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarefullyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarefullyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/carefully/CarefullyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carefully;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarefullyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
