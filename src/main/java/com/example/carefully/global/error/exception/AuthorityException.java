490a88ee7a8ef2ccd6660fbea1d38af900908603
semi-cloud <sksmsrkdtpal@naver.com>
2022-07-15 00:28:58 +0900

    :sparkles: 기본 예외 처리 구조 생성 (#1)

---
+++ src/main/java/com/example/carefully/global/error/exception/AuthorityException.java
@@ -0,0 +1,16 @@
+package com.example.carefully.global.error.exception;
+
+import com.example.carefully.global.error.common.ErrorCode;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
+public class AuthorityException extends BaseException {
+    public AuthorityException(ErrorCode errorCode){
+        super(errorCode);
+    }
+
+    public AuthorityException(String message, ErrorCode errorCode){
+        super(message, errorCode);
+    }
+}
