e939f558cca9da1d85e8605c055962412b42b063
semi-cloud <sksmsrkdtpal@naver.com>
2022-08-09 22:53:13 +0900

    :sparkles: 글 수정 기능 추가 (#5)

--- src/main/java/com/example/carefully/domain/post/domain/Post.java
+++ src/main/java/com/example/carefully/domain/post/domain/Post.java
@@ -37,4 +37,9 @@ public class Post extends BaseEntity {
         this.content = content;
         this.postRole = postRole;
     }
+
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
 }
