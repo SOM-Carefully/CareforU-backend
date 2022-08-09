a49b3a8b1397fe51d9b2dc6356abc0a55e9b722b
semi-cloud <sksmsrkdtpal@naver.com>
2022-08-09 23:46:12 +0900

    :sparkles: 글 상세정보 및 리스트 조회 기능 추가 (#5)

--- src/main/java/com/example/carefully/domain/post/service/PostService.java
+++ src/main/java/com/example/carefully/domain/post/service/PostService.java
@@ -1,8 +1,14 @@
 package com.example.carefully.domain.post.service;
 
 import com.example.carefully.domain.post.dto.PostDto;
+import com.example.carefully.global.dto.SliceDto;
+import org.springframework.data.domain.Pageable;
 
 public interface PostService {
     PostDto.CreateResponse createNewPost(PostDto.CreateRequest request, String postRole);
     PostDto.UpdateResponse updatePost(PostDto.UpdateRequest request, Long postId);
+
+    PostDto.SearchResponse searchPostDetail(Long postId);
+
+    SliceDto<PostDto.SearchResponse> searchPostList(String postRole, Pageable pageable);
 }
