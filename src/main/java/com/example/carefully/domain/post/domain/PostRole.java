f8efec65b3e584149cbc22b41a613f28aade0bb2
semi-cloud <sksmsrkdtpal@naver.com>
2022-08-09 22:37:57 +0900

    :sparkles: 글 생성 기능 추가 (#5)

---
+++ src/main/java/com/example/carefully/domain/post/domain/PostRole.java
@@ -0,0 +1,11 @@
+package com.example.carefully.domain.post.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+
+@Getter
+@RequiredArgsConstructor
+public enum PostRole {
+    SERVICE, NOTICE, QUEST, FREE
+}
