39e0a5dec3651dc02e7e5e434ec6fe0019e93111
myunbongs <62318430+myunbongs@users.noreply.github.com>
2022-08-12 10:41:07 +0900

    Revert ":sparkles: 기본 예외 처리 구조 생성 (#1)"
    This reverts commit 81625e1236a00a65b860cc2c24522568d3e65f37.

--- src/main/java/com/example/carefully/global/error/exception/UserStatusException.java
+++
@@ -1,9 +0,0 @@
-package com.example.carefully.global.error.exception;
-
-import com.example.carefully.global.error.common.ErrorCode;
-
-public class UserStatusException extends BaseException{
-    public UserStatusException(ErrorCode errorCode) {
-        super(errorCode);
-    }
-}
