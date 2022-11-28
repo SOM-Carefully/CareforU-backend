3da5b43d9bdbfa0c7216af441d3130db5c66ebf1
beeeeni <hyunbini2942@gmail.com>
2022-11-28 19:28:40 +0900

    :sparkles: cors 설정 추가

--- src/main/java/com/example/carefully/CarefullyApplication.java
+++ src/main/java/com/example/carefully/CarefullyApplication.java
@@ -16,4 +16,13 @@ public class CarefullyApplication {
         SpringApplication.run(CarefullyApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
 }
