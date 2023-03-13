a59a1937a07c5476df38a81eebc1b1239a5979e7
Semi kang <71436576+semi-cloud@users.noreply.github.com>
2023-03-13 12:08:08 +0900

    :recycle: JPA N+1 문제 해결 및 배치 사이즈 설정 (#77) (#78)
    * :sparkles: @EntityGraph을 이용한 페치 조인으로 변경 (#77)
    
    * :closed_lock_with_key: 배치 사이즈 설정 추가 (#77)
    
    * :twisted_rightwards_arrows: 충돌 머지 (#77)

--- src/main/java/com/example/carefully/domain/membership/repository/MembershipRepository.java
+++ src/main/java/com/example/carefully/domain/membership/repository/MembershipRepository.java
@@ -5,10 +5,14 @@ import com.example.carefully.domain.membership.entity.MembershipStatus;
 import com.example.carefully.domain.user.entity.User;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MembershipRepository extends JpaRepository<Membership, Long> {
     Slice<Membership> findAllByMembershipStatusOrderByCreatedAtDesc(Pageable pageable, MembershipStatus membershipStatus);
+
+    @EntityGraph(attributePaths = {"user", "admin"})
     Slice<Membership> findAllByOrderByCreatedAtDesc();
+
     Slice<Membership> findAllByUser(User user);
 }
