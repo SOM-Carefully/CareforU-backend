00522dbab50083f8cafe446cefc933e29704b61c
semi-cloud <sksmsrkdtpal@naver.com>
2023-03-13 15:14:16 +0900

    :recylce: 기본 예외 클래스 생성자 수정 (#62)

--- src/main/java/com/example/carefully/global/error/exception/UserStatusException.java
+++ src/main/java/com/example/carefully/global/error/exception/UserStatusException.java
@@ -2,7 +2,7 @@ package com.example.carefully.global.error.exception;
 
 import com.example.carefully.global.error.common.ErrorCode;
 
-public class UserStatusException extends BaseException{
+public class UserStatusException extends BaseException {
     public UserStatusException(ErrorCode errorCode) {
         super(errorCode);
     }
