96f697d728235931785f6a6b6a9ccf870f2b0be7
semi-cloud <sksmsrkdtpal@naver.com>
2023-03-24 10:59:42 +0900

    :adhesive_bandage: cors 프론트 배포 경로 허용

--- src/main/java/com/example/carefully/global/security/config/WebMvcConfig.java
+++ src/main/java/com/example/carefully/global/security/config/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "http://3.37.196.234:3000")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "OPTIONS", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
